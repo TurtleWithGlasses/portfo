77e316db8f88ec3fb7e92f75730112e70068412e
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-05-14 16:25:47 +0300

    Updated info

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect
 import csv
 
 app = Flask(__name__)
 print(__name__)
 
 
-@app.route("/index.html")
+@app.route("/")
 def ny_home():
     return render_template("index.html")
+
 
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
 
 def write_to_file(data):
     with open("database.txt", mode="a") as database:
